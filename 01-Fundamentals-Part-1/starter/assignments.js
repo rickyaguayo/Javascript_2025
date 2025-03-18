91a07d69b4b1fcac314b7e6061230a2d631bdbd8
Ricky Aguayo <ricky.webdev@gmail.com>
2025-03-18 13:34:10 -0600

    more progress on fundamentals

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+let country = "USA";
+let continent = "North America";
+let population = "335 million";
+
+console.log(
+  `Ricky lives in ${country} in ${continent} with ${population} other people`
+);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,8 @@
+let ricky = "bored";
+
+console.log(40 + 8 + 23 - 10);
+console.log("Ricky");
+console.log(23);
+
+let firstName = "Ricky";
+console.log(firstName);
