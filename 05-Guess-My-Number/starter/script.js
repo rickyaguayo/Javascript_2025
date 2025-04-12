f603b802407472e0b2b5b2dc58b3cf1dbf7a1471
Ricky Aguayo <ricky.webdev@gmail.com>
2025-04-12 14:18:23 -0600

    manipulating css

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -21,10 +21,17 @@ document.querySelector('.check').addEventListener('click', () => {
   let score = Number(document.querySelector('.score').textContent);
   // console.log(guess, typeof guess);
 
+  // no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No number!';
+
+    // player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too high!';
@@ -34,6 +41,8 @@ document.querySelector('.check').addEventListener('click', () => {
       document.querySelector('.message').textContent = '💥 You lost the game';
       document.querySelector('.score').textContent = 0;
     }
+
+    // guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too low!';
