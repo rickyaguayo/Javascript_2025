2ee025079f8320e14b716461d5e2ebcd79fc5a3b
Ricky Aguayo <ricky.webdev@gmail.com>
2025-05-19 11:56:28 -0600

    scoping in practive

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -18,11 +18,45 @@
 //   console.log(d + a);
 // }
 
-let a = true;
+// let a = true;
 
-if (a) {
-  var b = 'ricky';
-} else {
-  var c = 'not ricky';
+// if (a) {
+//   var b = 'ricky';
+// } else {
+//   var c = 'not ricky';
+// }
+// console.log(a, b, c);
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}.`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+
+      // new variable for with same name as outer scope
+      const firstName = 'Bibi';
+
+      // reassigning outerscope variable
+      output = 'new output';
+
+      const str = `Oh and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+      console.log(millenial);
+      console.log(output);
+      console.log(add(2, 3));
+    }
+  }
+  printAge();
+  return age;
 }
-console.log(a, b, c);
+
+const firstName = 'Ricky';
+calcAge(1992);
