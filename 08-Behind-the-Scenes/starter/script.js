30ce5ad84cea75e32a4b5fc67c9e6a262f0dbec3
Ricky Aguayo <ricky.webdev@gmail.com>
2025-05-19 11:15:55 -0600

    scoping lesson

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+// const a = 'ricky';
+// first();
+
+// function first() {
+//   const b = 'hello';
+//   second();
+
+//   function second() {
+//     const c = 'Hi';
+//     third();
+//   }
+// }
+
+// function third() {
+//   const d = 'hey!';
+//   console.log(d + a);
+// }
+
+let a = true;
+
+if (a) {
+  var b = 'ricky';
+} else {
+  var c = 'not ricky';
+}
+console.log(a, b, c);
