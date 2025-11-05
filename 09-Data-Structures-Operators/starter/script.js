9b00f2684df022cc7d9ade95d88013db13b3eeb3
Ricky Aguayo <ricky.webdev@gmail.com>
2025-11-05 14:06:22 -0700

    for-of loops and enhancing object literals

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -23,35 +23,47 @@ const mexicanFoods = new Set([
 ]);
 
 // Data needed for first part of the section
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours,
 
-  order: function (starterIndex, mainIndex) {
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDeliver: function ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = '20:00',
-    address,
-  }) {
+  orderDeliver({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
     console.log(
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
 
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
 
-  orderPizza: function (mainIng, ...otherIngs) {
+  orderPizza(mainIng, ...otherIngs) {
     console.log(mainIng, otherIngs);
   },
 
@@ -82,6 +94,22 @@ const rest2 = {
 };
 
 //////////////////////////////////
+//////ENHANCED OBJECCT LITERALS///////
+
+/*
+//////////////////////////////////
+//////FOR-OF LOOP///////
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+for (const item of menu) {
+  console.log(item);
+}
+
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
+}
+
+// console.log(...menu.entries());
+//////////////////////////////////
 //////LOGICAL ASSIGNMENT OPERATORS///////
 
 // OR assignment operator
@@ -110,7 +138,6 @@ console.log(rest2);
 
 //////////////////////////////////
 //////SHORT CIRCUITING///////
-/*
 // || operator
 console.log(3 || 'ricky');
 console.log('' || 'ricky');
